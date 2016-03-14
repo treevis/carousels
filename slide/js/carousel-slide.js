13965c790e9bfbeedec36910b8fa2ceaa9d8a48b
Travis Forden <travis@treevis.com>
2016-03-14 10:48:27 -0700

    Clean up slide carousel

--- slide/js/carousel-slide.js
+++ slide/js/carousel-slide.js
@@ -1,38 +1,86 @@
-/* Carousel */
-$(document).ready(function() {
-	var $content = $("#content");
-	$content.addClass( 'fancy' );
+// Carousel, Click Version
+(function($, undefined) {
 	
-	var tallestContentItem = 0;
+	var $carousel = $('.carousel');
+	if( !$carousel.length ) {
+		return;
+	}
 	
-	var $contentItems = $content.children("li");
+	var $carouselItems = $carousel.find('li'),
+		carouselItemsLength = $carouselItems.length,
+		slices = carouselItemsLength,
+		currentSlice = 1,
+		tallestContentItem = 0;
 	
-	$contentItems.each(function() {
-		var ind = $("#content li").index(this);
-		var leftPos = 24 * ind;
+	// Add Controls
+	function addControls() {
+		var $buttons = $('<div class="controls"><button type="button" class="next">&raquo;</button><button type="button" class="prev" disabled="disabled">&laquo;</button></div>');
 		
-		var height = $(this).height();
-		if( height > tallestContentItem ) {
-			tallestContentItem = height;
+		$buttons.find('button.next').click(function() {
+			advanceItems($(this),'forward');
+		});
+	
+		$buttons.find('button.prev').click(function() {
+			advanceItems($(this),'backward');
+		});
+		
+		return $buttons;
+	}
+	
+	function advanceItems(btn, dir) {
+		if( dir == 'forward' ) {
+			currentSlice++;
+			if( currentSlice > slices ) {
+				currentSlice = slices;
+			}
+			$carouselItems.animate({"left": "-=16em"}, "slow");
 		}
-		$(this).css({ position: "absolute", left: leftPos+"em" }).addClass("s"+ind);
-	});
+		else if( dir == 'backward' ) {
+			currentSlice--;
+			if( currentSlice < 1 ) {
+				currentSlice = 1;
+			}
+			$carouselItems.animate({"left": "+=16em"}, "slow");
+		}
 	
-	$contentItems.css("height", tallestContentItem);
+		if( currentSlice == slices ) { // disable/hide next
+			$('button.next').attr('disabled','disabled');
+			$('button.prev').removeAttr('disabled','disabled');
+		}
+		else if( currentSlice == 1 ) { // disable/hide previous
+			$('button.prev').attr('disabled','disabled');
+			$('button.next').removeAttr('disabled','disabled');
+		}
+		else { // show both
+			$('button.next').removeAttr('disabled','disabled');
+			$('button.prev').removeAttr('disabled','disabled');
+		}
+	}
 	
-	// Navigation
-	$("#content").before('<ul id="nav" class="clearfix"><li class="prev"><button type="button" name="previous">&laquo;<\/button><\/li><li class="next"><button type="button" name="next">&raquo;<\/button><\/li><\/ul>');
 	
-	var $previous = $("#nav li.prev button");
-	var $next = $("#nav li.next button");
+	// Initialize carousel
+	function init() {
+		$('html').addClass('js');
+		
+		if( slices > 1 ) {
+			var $buttons = addControls();
+			$carousel.append( $buttons );
+
+			// Prepare each carousel item, calculating the height of the tallest one
+			$carouselItems.each(function(index) {
+				var ind = index,
+					leftPos = 16 * ind,
+					height = $(this).height();
+				
+				if( height > tallestContentItem ) {
+					tallestContentItem = height;
+				}
+				$(this).css({ left: leftPos+"em" }).addClass("s"+ind);
+			});
 	
-	$previous.click(function(evt) {
-		evt.preventDefault();
-		$contentItems.animate({"left": "+=24em"}, "slow");
-	});
-	$next.click(function(evt) {
-		evt.preventDefault();
-		$contentItems.animate({"left": "-=24em"}, "slow");
-	});
-	
-});
+			$carousel.find("ul").css("height", tallestContentItem);
+		}
+	}
+
+	init();
+})(jQuery);
